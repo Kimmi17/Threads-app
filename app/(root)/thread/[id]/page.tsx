5f8ae88a5129211aff934692a07ed9ccfb2d2904
Kimmi17 <dangthikimthuybl@gmail.com>
2023-08-16 07:18:52 +0300

    Creating Profile Page.

--- app/(root)/thread/[id]/page.tsx
+++ app/(root)/thread/[id]/page.tsx
@@ -21,8 +21,9 @@ const Page = async ({ params }: { params: { id: string } }) => {
     <section className="relative">
       <div>
         <ThreadCard
+          key={thread._id}
           id={thread._id}
-          currentUserId={user.id}
+          currentUserId={user?.id || ""}
           parentId={thread.parentId}
           content={thread.text}
           author={thread.author}
@@ -43,7 +44,7 @@ const Page = async ({ params }: { params: { id: string } }) => {
           <ThreadCard
             key={childItem._id}
             id={childItem._id}
-            currentUserId={user.id}
+            currentUserId={childItem?.id || ""}
             parentId={childItem.parentId}
             content={childItem.text}
             author={childItem.author}
