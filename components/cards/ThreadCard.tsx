86c354acf0cc035c54c57c67fdbb520e8d51e4f9
Kimmi17 <dangthikimthuybl@gmail.com>
2023-08-18 01:53:34 +0300

    Enhanced comment display in ThreadCard.

--- components/cards/ThreadCard.tsx
+++ components/cards/ThreadCard.tsx
@@ -124,6 +124,27 @@ const ThreadCard = ({
           isComment={isComment}
         />
       </div>
+
+      {!isComment && comments.length > 0 && (
+        <div className="ml-1 mt-3 flex items-center gap-2">
+          {comments.slice(0, 2).map((comment, index) => (
+            <Image
+              key={index}
+              src={comment.author.image}
+              alt={`user_${index}`}
+              width={24}
+              height={24}
+              className={`${index !== 0 && "-ml-5"} rounded-full object-cover`}
+            />
+          ))}
+
+          <Link href={`/thread/${id}`}>
+            <p className="mt-1 text-subtle-medium text-gray-1">
+              {comments.length} repl{comments.length > 1 ? "ies" : "y"}
+            </p>
+          </Link>
+        </div>
+      )}
       {/*TODO: Show comment logo*/}
       {!isComment && community && (
         <Link
